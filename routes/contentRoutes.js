c9b49d6c149b3dafcebb2d65deeca414934996fc
cafer <caferkilinc1206@gmail.com>
2025-12-25 18:25:47 +0300

    delete

--- routes/contentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const contentController = require("../controllers/contentController");
-
-router.post("/generate", contentController.generateContent);
-
-module.exports = router;
